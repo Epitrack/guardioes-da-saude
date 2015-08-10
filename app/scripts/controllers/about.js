f36f470ef09be91e3f7df4d31ad9e9d6a3016c37
denniscalazans <denniscalazans@gmail.com>
2015-08-10 17:40:40 -0300

    Yeoman biolerplate files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guardioesDaSaudeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the guardioesDaSaudeApp
+ */
+angular.module('guardioesDaSaudeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guardioesDaSaudeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the guardioesDaSaudeApp
+ */
+angular.module('guardioesDaSaudeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
