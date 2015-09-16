57b0bb6a99fa136bb1342ef0b12436740ade526d
thulioph <thulioph@gmail.com>
2015-09-16 19:18:36 -0300

    Change primary route to login.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,9 +20,9 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
