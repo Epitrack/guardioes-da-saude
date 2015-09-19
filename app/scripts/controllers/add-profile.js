15a98cda9c90ad2bcfe2411e7a2e404c8006c135
thulioph <thulioph@gmail.com>
2015-09-19 03:22:22 -0300

    Adds new route.

---
+++ app/scripts/controllers/add-profile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gdsApp.controller:AddProfileCtrl
+ * @description
+ * # AddProfileCtrl
+ * Controller of the gdsApp
+ */
+angular.module('gdsApp')
+  .controller('AddProfileCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
