83ea1d4c7e5093bc8371e6bb0a637f10f21d25bb
Luciano Lucena <luc.allucena@gmail.com>
2015-10-29 11:28:35 -0300

    Implementing new chart styles and elements on health-daily page

--- app/scripts/controllers/health-daily.js
+++ app/scripts/controllers/health-daily.js
@@ -30,27 +30,27 @@ angular.module('gdsApp')
 
 	  $scope.lineOptions = {
 	    data: [
-	      { y: '2006', a: 100, b: 90 },
-	      { y: '2007', a: 75,  b: 65 },
-	      { y: '2008', a: 50,  b: 40 },
-	      { y: '2009', a: 75,  b: 65 },
-	      { y: '2010', a: 50,  b: 40 },
-	      { y: '2011', a: 75,  b: 65 },
-	      { y: '2012', a: 100, b: 90 }
+	      { y: '2006', a: 10, b: 90 },
+	      { y: '2007', a: 45,  b: 65 },
+	      { y: '2008', a: 30,  b: 40 },
+	      { y: '2009', a: 55,  b: 65 },
+	      { y: '2010', a: 10,  b: 40 },
+	      { y: '2011', a: 55,  b: 65 },
+	      { y: '2012', a: 45, b: 90 }
 	    ],
 	    xkey: 'y',
-	    ykeys: ['a', 'b'],
-	    labels: ['Series A', 'Series B'],
+	    ykeys: ['a'],
+	    labels: ['Total'],
+	    lineColors: ['#1E88E5'],
 	    resize: true
 	  };
 
 	  $scope.donutOptions = {
 		  data: [
-		    {label: "Download Sales", value: 12},
-		    {label: "In-Store Sales", value: 30},
-		    {label: "Mail-Order Sales", value: 20},
+		    {label: "Bem", value: 77},
+		    {label: "Mal", value: 23}
 		  ],
-		  resize: true
+		  colors: ['#E0D433', '#C81204']
 		};
 
 		$scope.areaOptions = {
