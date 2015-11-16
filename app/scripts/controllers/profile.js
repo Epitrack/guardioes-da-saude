2f4b3216000fcd90fe6484f910bffa62c3e2b81f
thulioph <thulioph@gmail.com>
2015-11-15 23:27:59 -0300

    Get user of rootScope.

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,18 +8,42 @@
  * Controller of the gdsApp
  */
 angular.module('gdsApp')
-  .controller('ProfileCtrl', ['$scope', 'LocalStorage', 'UserApi', '$rootScope', function ($scope, LocalStorage, UserApi, $rootScope) {
+  .controller('ProfileCtrl', ['$scope', 'UserApi', '$rootScope', 'toaster', function ($scope, UserApi, $rootScope, toaster) {
+
     $scope.pageClass = 'profile-page';
 
-    $scope.user = {};
+    // set user with $rootScope data
+    $scope.getUser = function() {
+      var u = $rootScope.user;
 
-    // set user with locaStorage data
-    $scope.user = LocalStorage.getItem('userStorage');
+      $scope.screen = {};
+
+      $scope.screen.user = {
+        nick: u.nick,
+        dob: u.dob,
+        gender: u.gender,
+        email: u.email,
+        race: u.race,
+        password: ""
+      };
+    };
 
     $scope.editProfile = function() {
-      UserApi.updateProfile($scope.user, function(data) {
-        console.log(data);
+      if($scope.screen.user.password == "" || $scope.screen.user.password != $scope.screen.repeatPassword) {
+        delete $scope.screen.user.password;
+      }
+
+      UserApi.updateProfile($scope.screen.user, function(data) {
+        console.log('editProfile: ', data);
+        if (data.data.error === true) {
+          toaster.pop('error', data.data.message);
+        } else {
+          toaster.pop('success', data.data.message);
+        }
+
+        $scope.getUser();
       });
     };
 
+    $scope.getUser();
   }]);
