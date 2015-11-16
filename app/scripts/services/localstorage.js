d2983793f840241af5dafc16ae064b1cac0542fb
thulioph <thulioph@gmail.com>
2015-11-15 23:59:30 -0300

    Adds into rootScope, localStorage data.

--- app/scripts/services/localstorage.js
+++ app/scripts/services/localstorage.js
@@ -8,7 +8,7 @@
  * Service in the gdsApp.
  */
 angular.module('gdsApp')
-  .service('LocalStorage', function () {
+  .service('LocalStorage', function ($rootScope) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var obj = {};
 
@@ -45,6 +45,8 @@ angular.module('gdsApp')
       userStorage.user_token = obj.token;
 
       // adiciona no storage as informações do usuário
+      $rootScope.user = userStorage;
+
       localStorage.setItem('userStorage', JSON.stringify(userStorage));
     };
 
@@ -76,12 +78,30 @@ angular.module('gdsApp')
       userStorage.zip = obj.zip;
       userStorage.user_token = token;
 
+      $rootScope.user = userStorage;
+
       localStorage.setItem('userStorage', JSON.stringify(userStorage));
     };
 
-    obj.updateUser = function(data) {
-      // adiciona no storage as informações do usuário
-      localStorage.setItem('userStorageUpdate', JSON.stringify(data.data.data[0]));
+    obj.updateUser = function(user) {
+      // adds into storage user info/data
+      if(user.data.data.length > 0) {
+        var newUser = user.data.data[0];
+        var currentUser = obj.getItem("userStorage");
+
+        // console.log("newUser", newUser);
+        // console.log("currentUser", currentUser);
+
+        angular.forEach(newUser, function(v, key){
+          currentUser[key] = v;
+        });
+
+        // console.log("currentUser M", currentUser);
+
+        $rootScope.user = currentUser;
+      }
+
+      localStorage.setItem('userStorage', JSON.stringify(currentUser));
     };
 
     return obj;
