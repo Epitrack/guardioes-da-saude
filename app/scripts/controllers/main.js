3069c959161580affb43b28396178683d7813d27
thulioph <thulioph@gmail.com>
2015-11-16 03:53:25 -0300

    Removes user from localStorage and get rootScope.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,11 @@
  * Controller of the gdsApp
  */
 angular.module('gdsApp')
-  .controller('MainCtrl', ['$scope', 'LocalStorage', '$route', function ($scope, LocalStorage, $route) {
+  .controller('MainCtrl', ['$scope', '$route', function ($scope, $route) {
+
     $scope.pageClass = 'main-page';
 
     console.log($route);
-
-    // set user with locaStorage data
-    $scope.user = LocalStorage.getItem('userStorage');
-
-    if ($scope.user != null) {
-      $scope.household = $scope.user.household;
-    }
-    // ====
 
     // when user click in logout button
     $scope.clearStorage = function() {
