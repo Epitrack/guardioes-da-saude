a0a8e3a856c16ec9681496fd523ff35d7e149162
Victor Pereira Luna <victorluna22@gmail.com>
2015-11-29 14:17:05 -0300

    change url base

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -8,7 +8,7 @@ angular.module("gdsApp")
   delete $httpProvider.defaults.headers.common["X-Requested-With"]
 }])
 .constant('ApiConfig', {
-  'API_URL': 'http://52.20.162.21',
+  'API_URL': 'http://api.guardioesdasaude.org',
   'APP_TOKEN': 'd41d8cd98f00b204e9800998ecf8427e',
   'PLATFORM': 'web',
   'CLIENT': 'api'
