6e669865a2726ad20143e76e1cb2536522e9bf4f
thulioph <thulioph@gmail.com>
2016-01-15 15:04:07 -0300

    Show graphics.

--- app/scripts/controllers/health-daily.js
+++ app/scripts/controllers/health-daily.js
@@ -98,34 +98,34 @@ angular.module('gdsApp')
     // ====
 
     // ====
-    // $scope.graphic = function() {
-    //   $scope.lineOptions = {
-    //     data: [
-    //       { y: '2006', a: 10, b: 90 },
-    //       { y: '2007', a: 45,  b: 65 },
-    //       { y: '2008', a: 30,  b: 40 },
-    //       { y: '2009', a: 55,  b: 65 },
-    //       { y: '2010', a: 10,  b: 40 },
-    //       { y: '2011', a: 55,  b: 65 },
-    //       { y: '2012', a: 45, b: 90 }
-    //     ],
-    //     xkey: 'y',
-    //     ykeys: ['a'],
-    //     labels: ['Total'],
-    //     lineColors: ['#1E88E5'],
-    //     resize: true
-    //   };
+    $scope.graphic = function() {
+      $scope.lineOptions = {
+        data: [
+          { y: '2006', a: 10, b: 90 },
+          { y: '2007', a: 45,  b: 65 },
+          { y: '2008', a: 30,  b: 40 },
+          { y: '2009', a: 55,  b: 65 },
+          { y: '2010', a: 10,  b: 40 },
+          { y: '2011', a: 55,  b: 65 },
+          { y: '2012', a: 45, b: 90 }
+        ],
+        xkey: 'y',
+        ykeys: ['a'],
+        labels: ['Total'],
+        lineColors: ['#1E88E5'],
+        resize: true
+      };
 
-    //   $scope.donutOptions = {
-    //     data: [
-    //       {label: "Bem", value: 77, participants: 10},
-    //       {label: "Mal", value: 23, participants: 5}
-    //     ],
-    //     colors: ['#E0D433', '#C81204'],
-    //     resize: true
-    //   };
-    // };
+      $scope.donutOptions = {
+        data: [
+          {label: "Bem", value: 77, participants: 10},
+          {label: "Mal", value: 23, participants: 5}
+        ],
+        colors: ['#E0D433', '#C81204'],
+        resize: true
+      };
+    };
 
-    // $scope.graphic();
+    $scope.graphic();
     // ====
   }]);
