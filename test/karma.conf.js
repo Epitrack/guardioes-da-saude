c722849daedf39d7c101bc1f9a18b844f5a581d4
thulioph <thulioph@gmail.com>
2016-02-11 12:00:28 -0300

    Adjusts calendar and navbar width

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/moment/moment.js',
       'bower_components/underscore/underscore.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
       'bower_components/raphael/raphael.js',
