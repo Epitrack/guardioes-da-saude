5749eef8ec5924ac11181285f75196f2dea29a51
squaq <evertonsquaq@gmail.com>
2016-02-22 23:44:47 -0300

    new map added

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -52,6 +52,9 @@ module.exports = function(config) {
       'bower_components/ngmap/build/scripts/ng-map.js',
       'bower_components/ng-facebook/ngFacebook.js',
       'bower_components/angular-repeat-n/dist/angular-repeat-n.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
