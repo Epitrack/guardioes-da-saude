5a0e1e0c460909cbcceba1c008d3587692b92802
thulioph <thulioph@gmail.com>
2016-03-03 10:11:55 -0300

    Adjusts notification service.

--- app/scripts/services/notification.js
+++ app/scripts/services/notification.js
@@ -11,24 +11,24 @@ angular.module('gdsApp')
   .service('Notification', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    function getStatus () {
-      if (!window.Notification) {
-        return "unsupported";
-      }
-      return window.Notification.permission;
-    }
+    // function getStatus () {
+    //   if (!window.Notification) {
+    //     return "unsupported";
+    //   }
+    //   return window.Notification.permission;
+    // }
 
-    function getPermission () {
-      return new Promise((resolve, reject) => {
-        Notification.requestPermission(status => {
-          if (status == 'granted') {
-            resolve();
-          }else{
-            reject(status);
-          }
-        });
-      });
-    }
+    // function getPermission () {
+    //   return new Promise((resolve, reject) => {
+    //     Notification.requestPermission(status => {
+    //       if (status == 'granted') {
+    //         resolve();
+    //       }else{
+    //         reject(status);
+    //       }
+    //     });
+    //   });
+    // }
 
     var obj = {};
 
