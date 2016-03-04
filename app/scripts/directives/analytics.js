7d2f45bb9b19afc6e89e19cabb30bc825c177ed2
thulioph <thulioph@gmail.com>
2016-03-04 20:31:41 -0300

    Adds analytics code.

--- app/scripts/directives/analytics.js
+++ app/scripts/directives/analytics.js
@@ -16,12 +16,11 @@ angular.module('gdsApp')
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('send', 'pageview');
-
         var id;
 
         scope.$watch(
           function() {
+            ga('send', 'pageview');
             return id;
           },
 
