e6ea57538573f2e99b329d0e47f9e6d0e1d0e716
squaq <evertonsquaq@gmail.com>
2016-03-29 16:52:25 -0300

    map clusters

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -46,6 +46,7 @@ module.exports = function(config) {
       'bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js',
       'bower_components/ngmap/build/scripts/ng-map.js',
       'bower_components/ng-facebook/ngFacebook.js',
+      'bower_components/js-marker-clusterer/src/markerclusterer.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
