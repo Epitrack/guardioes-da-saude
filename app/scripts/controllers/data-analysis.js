84611f6d32a7e7b01768b4f854c518ece1eba331
João Gabriel Lima <jgabriel.ufpa@gmail.com>
2016-04-01 00:35:40 -0300

    ajustes e merge

--- app/scripts/controllers/data-analysis.js
+++ app/scripts/controllers/data-analysis.js
@@ -8,10 +8,59 @@
  * Controller of the gdsApp
  */
 angular.module('gdsApp')
-  .controller('DataAnalysisCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('DataAnalysisCtrl', function (Surveyapi, $scope) {
+
+    // ====
+    // Get all symptoms
+    $scope.dash = {};
+
+    Surveyapi.getSymptoms(function (data) {
+      $scope.symptomsList = data.data.data;
+    });
+    // ====
+
+    // ====
+    // Graphic types carousel
+    function _addSlide(){
+      var slides = $scope.slides = [
+        {
+          image: '../../images/dashboard/icon_pizza.svg',
+          text: 'Pizza',
+          id: 0
+        },
+        {
+          image: '../../images/dashboard/icon_histograma.svg',
+          text: 'Barra',
+          id: 1
+        },
+        {
+          image: '../../images/dashboard/icon_coluna.svg',
+          text: 'Histograma',
+          id: 2
+        },
+        {
+          image: '../../images/dashboard/icon_tabela.svg',
+          text: 'Tabela',
+          id: 3
+        }
+      ];
+    };
+
+    _addSlide();
+    // ====
+
+    // ====
+    // Range
+    $scope.range = {
+      min: 1,
+      max: 52
+    };
+    // ====
+
+    // ====
+    // Drag and drop
+    $scope.getGraphic = function() {
+      console.warn('Dashboard data -> ', $scope.dash)
+    };
+    // ====
   });
