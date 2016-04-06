a1202b08811d5f0db55a8b122aafbd6fc5cb1437
thulioph <thulioph@gmail.com>
2016-04-06 14:56:41 -0300

    Adds highcharts as a dependencie with bower.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -58,6 +58,9 @@ module.exports = function(config) {
       'bower_components/Sortable/ng-sortable.js',
       'bower_components/Sortable/knockout-sortable.js',
       'bower_components/Sortable/react-sortable-mixin.js',
+      'bower_components/highcharts/highcharts.js',
+      'bower_components/highcharts/highcharts-more.js',
+      'bower_components/highcharts/modules/exporting.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
