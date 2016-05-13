851c7657eeedfe133d6df91c16e25fe9c4771b2b
João Gabriel Lima <jgabriel.ufpa@gmail.com>
2016-05-13 12:55:26 -0300

    ajustes nos boxs dos alertas

--- app/scripts/controllers/dashboard-inteligencia.js
+++ app/scripts/controllers/dashboard-inteligencia.js
@@ -8,6 +8,17 @@
  * Controller of the gdsApp
  */
 angular.module('gdsApp')
-    .controller('DashboardInteligenciaCtrl', function($scope, $location, $rootScope, $compile) {
+    .controller('DashboardInteligenciaCtrl', function($scope, $location, $rootScope, $http, $compile, ApiConfig) {
+
+
+        var apiUrl = ApiConfig.API_URL;
+        var app_token = ApiConfig.APP_TOKEN;
+
+        $http.get(apiUrl + '/ei/alerts/')
+            .then(function(data) {
+                console.log('Success getAllData: ', data);
+            }, function(error) {
+                console.warn('Error getAllData: ', error);
+            });
 
     });
