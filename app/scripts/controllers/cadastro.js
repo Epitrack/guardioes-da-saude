1ca9d55cd4d0fbe76f98364837a6feea383b30ae
João Gabriel Lima <jgabriel.ufpa@gmail.com>
2016-06-08 14:22:20 -0300

    ajuste no login olympics

--- app/scripts/controllers/cadastro.js
+++ app/scripts/controllers/cadastro.js
@@ -19,9 +19,29 @@ angular.module('gdsApp')
       UserApi.facebookLogin($scope);
     };
 
-    $scope.googleLogin = function () {
-      UserApi.googleLogin($scope);
-    };
+    $scope.googleLogin = function() {
+            $scope.$on('event:google-plus-signin-success', function(event, authResult) {
+                console.log("google-plus-signin-success", event, authResult);
+                gapi.client.load('plus', 'v1', function() {
+                    var request = gapi.client.plus.people.get({
+                        'userId': 'me'
+                    });
+                    request.execute(function(resp) {
+                        try {
+                            console.log('Retrieved profile for:', resp, resp.emails[0].value);
+                            resp.email = resp.emails[0].value;
+                            UserApi.googleLogin($scope, resp);
+                        } catch (e) {
+                            alert("Não foi possíel realizar o login: " + JSON.stringify(e));
+                        }
+                    });
+                });
+
+            });
+            $scope.$on('event:google-plus-signin-failure', function(event, authResult) {
+                alert("Não foi possíel realizar o login");
+            });
+        };
 
     $scope.twitterLogin = function () {
       UserApi.twitterLogin($scope);
