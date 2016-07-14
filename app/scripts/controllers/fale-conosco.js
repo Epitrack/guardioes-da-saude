cc15b18ddbd7051181d70eda7b031abbf78bfd8e
João Gabriel <jgabriel.ufpa@gmail.com>
2016-07-14 17:24:58 -0300

    ajustes nas tasks #126305363 #126331889

--- app/scripts/controllers/fale-conosco.js
+++ app/scripts/controllers/fale-conosco.js
@@ -7,30 +7,106 @@
  * # FaleConoscoCtrl
  * Controller of the gdsApp
  */
-angular.module('gdsApp').controller('FaleConoscoCtrl', ['$scope', 'ContactApi', 'Notification', 
-  function ($scope, ContactApi, Notification) {
+angular.module('gdsApp').controller('FaleConoscoCtrl', ['$scope', 'ContactApi', 'Notification',
+    function($scope, ContactApi, Notification) {
 
-    $scope.contact = {};
+        $scope.contact = {};
 
-    $scope.sendContact = function () {
-      ContactApi.faleConosco($scope.contact, function (data) {
-        if (data.data.error === true) {
-          Notification.show('error', 'Contato', data.data.message);
-        } else {
-          Notification.show('success', 'Contato', data.data.message);
-          $scope.contact = {};
+        $scope.sendContact = function() {
+            $scope.contact.message += "\n\n\n\n" + $scope.returnBrowserOS();
+            // console.log(JSON.stringify($scope.contact));
+            ContactApi.faleConosco($scope.contact, function(data) {
+                if (data.data.error === true) {
+                    Notification.show('error', 'Contato', data.data.message);
+                } else {
+                    Notification.show('success', 'Contato', data.data.message);
+                    $scope.contact = {};
+                    window.location = "#/";
+                }
+            });
+        };
+
+        $scope.esterEgg = function() {
+            cheet('e p i t r a c k', function() {
+                angular.element('#modal-kc').modal({
+                    show: 'true'
+                });
+            });
+        };
+
+        $scope.returnBrowserOS = function() {
+
+            var RETORNO = "";
+            var nVer = navigator.appVersion;
+            var nAgt = navigator.userAgent;
+            var browserName = navigator.appName;
+            var fullVersion = '' + parseFloat(navigator.appVersion);
+            var majorVersion = parseInt(navigator.appVersion, 10);
+            var nameOffset, verOffset, ix;
+
+            // In Opera, the true version is after "Opera" or after "Version"
+            if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+                browserName = "Opera";
+                fullVersion = nAgt.substring(verOffset + 6);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+            // In MSIE, the true version is after "MSIE" in userAgent
+            else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+                browserName = "Microsoft Internet Explorer";
+                fullVersion = nAgt.substring(verOffset + 5);
+            }
+            // In Chrome, the true version is after "Chrome" 
+            else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+                browserName = "Chrome";
+                fullVersion = nAgt.substring(verOffset + 7);
+            }
+            // In Safari, the true version is after "Safari" or after "Version" 
+            else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+                browserName = "Safari";
+                fullVersion = nAgt.substring(verOffset + 7);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+            // In Firefox, the true version is after "Firefox" 
+            else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+                browserName = "Firefox";
+                fullVersion = nAgt.substring(verOffset + 8);
+            }
+            // In most other browsers, "name/version" is at the end of userAgent 
+            else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+                (verOffset = nAgt.lastIndexOf('/'))) {
+                browserName = nAgt.substring(nameOffset, verOffset);
+                fullVersion = nAgt.substring(verOffset + 1);
+                if (browserName.toLowerCase() == browserName.toUpperCase()) {
+                    browserName = navigator.appName;
+                }
+            }
+            // trim the fullVersion string at semicolon/space if present
+            if ((ix = fullVersion.indexOf(";")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+            if ((ix = fullVersion.indexOf(" ")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+
+            majorVersion = parseInt('' + fullVersion, 10);
+            if (isNaN(majorVersion)) {
+                fullVersion = '' + parseFloat(navigator.appVersion);
+                majorVersion = parseInt(navigator.appVersion, 10);
+            }
+
+            RETORNO += '\nBrowser= ' + browserName + ' ; ' + 'Version  = ' + fullVersion;
+
+            var OSName = "Unknown OS";
+            if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+            if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+            if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+            if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+
+            RETORNO += '\nOS: ' + OSName;
+            return RETORNO;
         }
-      });
-    };
 
-    $scope.esterEgg = function () {
-      cheet('e p i t r a c k', function () {
-        angular.element('#modal-kc').modal({
-          show: 'true'
-        });
-      });
-    };
+        // $scope.esterEgg();
 
-    // $scope.esterEgg();
-
-  }]);
+    }
+]);
