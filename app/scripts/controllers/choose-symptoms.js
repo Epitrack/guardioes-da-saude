56874498741313149821f283aa6c664c81079375
sfilhu <sfilhu@gmail.com>
2016-08-22 15:03:08 -0300

    Adjust hadHealthCare value true false

--- app/scripts/controllers/choose-symptoms.js
+++ app/scripts/controllers/choose-symptoms.js
@@ -95,6 +95,7 @@ angular.module('gdsApp').controller('ChooseSymptomsCtrl', ['$scope', '$translate
 
 
             // console.log(form);
+            
             if (isvalid) {
                 Surveyapi.submitSurvey(form, function(data) {
                     if (data.data.error === true) {
